a747fd4435f59f3c7e59613e4005fe863e299f42
entsuka70 <entsuka70@gmail.com>
2021-02-06 09:34:57 +0900

    feat: storybookに引き分けを追加

--- my-app/src/stories/Game.js
+++ my-app/src/stories/Game.js
@@ -3,11 +3,28 @@ import PropTypes from 'prop-types';
 import '../index.css';
 import {Board} from './Board';
 
-export const Game = ({classGame, classGameBoard, values}) => {
+export const Game = ({classGame, classGameBoard, values, classInfo, classStatus, status, history}) => {
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move:
+            'Go to game start';
+        return (
+            <li>
+                <button>{desc}</button>
+            </li>
+        )
+    });
+    
     return (
         <div className={classGame}>
             <div className={classGameBoard}>
                 {<Board value={values}/>}
+            </div>
+            <div className={classInfo}>
+                <div className={classStatus}>{status}</div>
+                <ol>
+                    {moves}
+                </ol>
             </div>
         </div>
     );
@@ -17,10 +34,18 @@ Game.propTypes = {
     classGame: PropTypes.string,
     classGameBoard: PropTypes.string,
     values: PropTypes.oneOf(['', '○', 'O', 'X', '△']),
+    classInfo: PropTypes.string,
+    classStatus: PropTypes.string,
+    status: PropTypes.oneOf(['Winner: ○', 'Winner: X', 'DRAW !!', 'Next player: X', 'Next player: ○']),
+    history: PropTypes.array,
 };
 
 Game.defaultProps = {
     classGame: 'game',
     classGameBoard: 'game-board',
     values: '△',
+    classInfo: 'game-info',
+    classStatus: 'status',
+    status: 'Next player: X',
+    history: [[],[],],
 };
